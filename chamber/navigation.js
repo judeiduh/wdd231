539268d8e2bb3ea5c73f56bfaffacba03e21ee56
judeiduh <judeiduh29@gmail.com>
2025-08-09 09:25:43 -0700

    delete

--- chamber/navigation.js
+++
@@ -1,8 +0,0 @@
-
-const hamburgerElement = document.querySelector('#myButton');
-const navElement = document.querySelector('.navigation');
-
-hamburgerElement.addEventListener('click', () => {
-    navElement.classList.toggle('open');
-    hamburgerElement.classList.toggle('open');
-});
--- chamber/scripts.js/sections.mjs
+++
@@ -1,9 +0,0 @@
-export function setSectionSelection(sections) {
-    const sectionSelect = document.querySelector("#sectionNumber");
-    sections.forEach((section) => {
-        const option = document.createElement("option");
-        option.value = section.sectionNumber;
-        option.textContent = `${section.sectionNumber}`;
-        sectionSelect.appendChild(option);
-    });
-}
